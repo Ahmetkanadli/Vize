49e12e03639faa606319c8189d5da37e0b64ee08
Ahmetkanadli <kanadliahmet5@gmail.com>
2023-11-16 22:37:01 +0300

    1.5 testleri ile tamamlandı

--- 210401071/Labex1_Q1_tests_210401071_ahmet_kanadli.R
+++ 210401071/Labex1_Q1_tests_210401071_ahmet_kanadli.R
@@ -1,34 +1,3 @@
-
-library(testthat)
-#ön hazırlık
-test_that("Çalışma alanındaki tüm değişkenleri sil", {
-  rm(list = ls())
-  expect_equal(length(ls()), 0)
-})
-
-test_that("Eğer mevcutsa veri setini sil", {
-  if ("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files()) {
-    file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
-  }
-  expect_equal("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files(), FALSE)
-})
-
-
-current_dir <- getwd()
-print(current_dir)
-relative_path <- file.path(current_dir, "Labex1_Q1_210401071_ahmet_kanadli.R")
-
-source(relative_path)
-
-#2.1
-test_that("Test : MapsThatChangedOurWorld_StoryMap_Data.csv adlı dosya belirtilen dizinde mevcuttur.", {
-  file_path <- file.path(current_dir,"MapsThatChangedOurWorld_StoryMap_Data.csv")
-  expect_true(file.exists(file_path), info = "Dosya mevcut değil.")
-})
-
-#2.2
-
-
 library(testthat)
 #ön hazırlık
 test_that("Çalışma alanındaki tüm değişkenleri sil", {
